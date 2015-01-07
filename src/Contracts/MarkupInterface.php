66b75b2e80be0cf3215c06d11ed708ef2a9d5b7d
ARCANEDEV <contact@arcanedev.net>
2015-01-07 16:42:02 +0000

    Implementing MarkupInterface

--- src/Contracts/MarkupInterface.php
+++ src/Contracts/MarkupInterface.php
@@ -1,6 +1,67 @@
 <?php namespace Arcanedev\Markup\Contracts;
 
+use Arcanedev\Markup\Contracts\Entities\TagInterface;
+
 interface MarkupInterface
 {
+    /* ------------------------------------------------------------------------------------------------
+     |  Main Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Create a new Tag (Alias)
+     *
+     * @param string $type
+     * @param array  $attributes
+     *
+     * @return TagInterface
+     */
+    public static function make($type = '', $attributes = []);
 
+    /* ------------------------------------------------------------------------------------------------
+     |  HTML Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Create TITLE
+     *
+     * @param string $text
+     *
+     * @return TagInterface
+     */
+    public static function title($text = '');
+
+    /**
+     * Create IMG Tag
+     *
+     * @param string $src
+     * @param string $alt
+     * @param array  $attributes
+     *
+     * @return TagInterface
+     */
+    public static function img($src, $alt = '', array $attributes = []);
+
+    /**
+     * Create META Tag
+     *
+     * @param string $name
+     * @param string $value
+     * @param string $content
+     * @param array  $attributes
+     *
+     * @return TagInterface
+     */
+    public static function meta($name = 'name', $value, $content, array $attributes = []);
+
+    /**
+     * Create ANCHOR Tag
+     *
+     * @param string $href
+     * @param string $text
+     * @param array  $attributes
+     *
+     * @return TagInterface
+     */
+    public static function link($href = '', $text = '', array $attributes = []);
 }
